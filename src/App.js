c7c61e3c41f7ef231fb28691523cae5b2b48f3df
sbi05016 <hys99271415@gmail.com>
2022-07-29 00:05:52 +0900

    apple

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {useState} from 'react';
 
 function App() {
   let [글제목, girl] = useState(['남자 코트 추천','고기국수 맛집 추천','국밥 맛집 추천']);
-  let [like, plus] = useState(0); 
+  let [like, plus] = useState([0,0,0]);
+  let [modal,setModal] = useState(false);
   
   
 
@@ -14,34 +15,59 @@ function App() {
       <div className='black-nav'>
         <h4>ReactBlog</h4>
       </div>
+      
+
+      <button onClick={()=>{      
+      let copy = [...글제목];
+      copy.sort()
+      girl(copy)
+        }}>정렬</button>
 
       <button onClick={()=>{
-      
       let copy = [...글제목];
       copy[0] = '여자코트 추천';
-      girl(copy);
+      girl(copy);      
       }}>글수정</button>
 
-      <div className="list">
-        <h4>
-          { 글제목[0] }
-          <span onClick={()=>{ plus(like+1) }}>❤️</span> {like}
+      
+    {
+      글제목.map((작명,i)=>{return(
+        <div className="list" key={i}>
+        <h4 onClick={()=>{setModal(!modal)}}>
+          { 작명 }
           
+          <span onClick={()=>{
+            let copy = [...like]
+            copy[i] = copy[i] +1
+            plus(copy)
+          }}>❤️</span> {like[i]}
         </h4>
         <p>2월 17일 방행</p>
-      </div>
-      <div className="list">
-        <h4>
-        { 글제목[1] } 
-        </h4>
-        <p>7월 07일 방행</p>
-      </div>
-      <div className="list">
-        <h4>{ 글제목[2] }</h4>
-        <p>12월 12일 방행</p>
-      </div>                  
+      </div>)
+            
+        }
+      )
+    }
+
+    {
+        modal==true ? <Modal/> : null
+      /* null은 비어있는 html용으로 자주 쓰임 */
+      }
     </div>
   );
 }
+
+      
+
+// 다른 function 바깥에 만들고, 영어 대문자
+const Modal=()=>{
+  return <div className="modal">
+  <h4>제목</h4>
+  <p>날짜</p>
+  <p>상세내용</p>  
+  </div>
+}
+
+
 
 export default App;
